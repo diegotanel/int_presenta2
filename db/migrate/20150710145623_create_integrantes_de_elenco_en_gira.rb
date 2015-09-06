5a674cb1c93d9fd640b5479c51bba0b53b22d5de
Martin <pereira.martin21@gmail.com>
2015-09-06 16:03:25 -0300

    Se arreglo que se creen los integrantes del elenco desde la ficha artistica

--- db/migrate/20150710145623_create_integrantes_de_elenco_en_gira.rb
+++ db/migrate/20150710145623_create_integrantes_de_elenco_en_gira.rb
@@ -1,22 +1,22 @@
 class CreateIntegrantesDeElencoEnGira < ActiveRecord::Migration
   def change
     create_table :integrantes_de_elenco_en_gira do |t|
-      t.string :nombre, :null => false
-      t.string :apellido, :null => false
+      t.string :nombre
+      t.string :apellido
       t.date :fecha_de_nacimiento
-      t.string :calle, :null => false
-      t.string :altura_calle, :null => false
+      t.string :calle
+      t.string :altura_calle
       t.string :piso
       t.string :depto
-      t.references :localidad, index: true, :null => false, foreign_key: true
-      t.string :codigo_postal, :null => false
+      t.references :localidad, index: true, foreign_key: true
+      t.string :codigo_postal
       t.string :tel_particular
       t.string :prefijo_tel_part
       t.string :prefijo_tel_cel
       t.string :tel_particular
       t.string :tel_celular
-      t.string :email, :null => false
-      t.references :elenco_en_gira, index: true, :null => false, foreign_key: true
+      t.string :email
+      t.references :elenco_en_gira, index: true, foreign_key: true
 
       t.timestamps
     end
